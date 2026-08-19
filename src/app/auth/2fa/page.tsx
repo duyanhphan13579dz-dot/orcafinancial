d6560f216cd586da6d6b45ea92b01492bd1caf96
duyanhphan13579dz-dot <duyanhphan13579dz@gmail.com>
2026-08-19 21:27:08 +0700

    Add TwoFactorPage component with login form

---
+++ src/app/auth/2fa/page.tsx
@@ -0,0 +1,10 @@
+import TwoFactorLoginForm from "./two-factor-login-form";
+
+export const dynamic =
+  "force-dynamic";
+
+export default function TwoFactorPage() {
+  return (
+    <TwoFactorLoginForm />
+  );
+}
